0eecc924b3b7211476b7182a7aed42f8945bb902
jorgmour <ilionez@hotmail.com>
2019-11-18 05:07:14 -0500

    Programacion

---
+++ Programacion.net/Programacion.net/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(92, 105)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "invierno"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(92, 292)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(102, 40)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "otoño"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(225, 105)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(416, 244)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(92, 169)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(102, 40)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "primavera"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(92, 234)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(102, 40)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "verano"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(242, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(378, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Programacion.net/Programacion.net/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\invierno.jpg")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\otoño.jpg")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\primavera.jpg")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\verano.jpg")
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        'MsgBox("La tecla a sido pulsada", MsgBoxStyle.Critical)'
+        'MsgBox(e.KeyChar)
+
+        If e.KeyChar = "1" Then
+            PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\invierno.jpg")
+        ElseIf e.KeyChar = "2" Then
+            PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\primavera.jpg")
+        ElseIf e.KeyChar = "3" Then
+            PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\otoño.jpg")
+        ElseIf e.KeyChar = "4" Then
+            PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\verano.jpg")
+        End If
+        MsgBox("La imagen a cambiado", MsgBoxStyle.Exclamation)
+        'SOLAMENTE PERMITE INGRESAR NUMEROS, LAS LETRAS QUEDAN BLOQUEADAS
+        If Char.IsNumber(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        ' If e.KeyCode = "112" Then
+        'PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\invierno.jpg")
+        'ElseIf e.KeyCode = "113" Then
+        'PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\primavera.jpg")
+        'ElseIf e.KeyCode = "114" Then
+        'PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\otoño.jpg")
+        'ElseIf e.KeyCode = "115" Then
+        'PictureBox1.Image = System.Drawing.Image.FromFile("C:\Users\Contabilidad\Desktop\Programacion .net\verano.jpg")
+        'End If
+    End Sub
+End Class
